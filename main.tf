9982923ede6ae2e6c9f9d5259756419a1d19e20a
nandhyalakarthikreddy <nandhyalakarthikreddy@gmail.com>
2025-11-14 12:50:34 +0530

    terraform commit

--- main.tf
+++ main.tf
@@ -4,4 +4,10 @@ module "vpc" {
     project_name = var.project_name
     Environment = var.Environment
     vpc_tags = var.vpc_tags
+
+        # public subnets
+    public_subnet_cidrs = var.public_subnet_cidrs
+
+    # private subnets
+    private_subnet_cidrs = var.private_subnet_cidrs
 }

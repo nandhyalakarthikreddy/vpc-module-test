a3fcb245f8a5d50902743adc0270bc62f0dc7403
nandhyalakarthikreddy <nandhyalakarthikreddy@gmail.com>
2025-11-14 11:35:00 +0530

    terraform commit

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "vpc" {
-    source = "../terraform-aws-vpc"
+    source = "../Terraform-aws-vpc"
     vpc_cidr = var.vpc_cidr
     project_name = var.project_name
     Environment = var.Environment
-    vpc_tags = var.vpc_tags
+    # vpc_tags = var.vpc_tags
 }
